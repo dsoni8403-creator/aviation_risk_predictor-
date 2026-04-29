679df1469ef9bd09ef6020ee2786bb2fa99a8ba3
krupa patel <dsoni8403@gmail.com>
2026-04-29 11:38:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+✈️ Aviation Risk Predictor
+Enter flight details:
+
+Weather
+
+Good (0)
+Flight Delay
+
+No (0)
+Maintenance Issue
+
+No (0)
+Pilot Experience (years)
+
+1
+
+
+Aircraft Age (years)
+
+1
